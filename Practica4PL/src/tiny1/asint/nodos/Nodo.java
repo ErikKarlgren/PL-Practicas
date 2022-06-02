eb8605b4bace7e5c5f504148d1a59d43e780e778
ErikKarlgren <erik.rivas8@gmail.com>
2022-06-03 01:09:59 +0200

    Implementado toString() para Nodo

--- Practica4PL/src/tiny1/asint/nodos/Nodo.java
+++ Practica4PL/src/tiny1/asint/nodos/Nodo.java
@@ -1,6 +1,12 @@
 package tiny1.asint.nodos;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 import tiny1.asint.nodos.tipos.Tipo;
+import tiny1.procesamientos.Impresion;
 import tiny1.procesamientos.Procesador;
 
 public abstract class Nodo {
@@ -29,5 +35,23 @@ public abstract class Nodo {
 
     public boolean esTipo() {
         return false;
+    }
+
+    @Override
+    public String toString() {
+        // Usamos el procesador de impresión para imprimir sobre un PrintStream,
+        // el cual creamos usando un ByteArrayOutputStream como buffer porque
+        // podemos usar el método toString() para obtener el resultado de la
+        // impresión como un String.
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String utf8 = StandardCharsets.UTF_8.name();
+
+        try (PrintStream ps = new PrintStream(baos, true, utf8)) {
+            this.procesa(new Impresion(ps));
+            return baos.toString(utf8);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
